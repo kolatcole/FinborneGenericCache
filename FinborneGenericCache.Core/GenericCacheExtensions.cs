b0ab2339394cc4fb435b807b7bf4bad0f8fce9d8
kolatcole <30967437+kolatcole@users.noreply.github.com>
2023-09-14 06:52:20 +0100

    All task completed

--- FinborneGenericCache.Core/GenericCacheExtensions.cs
+++ FinborneGenericCache.Core/GenericCacheExtensions.cs
@@ -8,19 +8,9 @@ namespace FinborneGenericCache.Core
     {
         public static IServiceCollection AddGenericCache<K,V>(this IServiceCollection services, GenericCacheConfig config)
         {
-
-            services.AddSingleton<IGenericCache<K, V>, GenericCache<K, V>>(provider =>
-            {
-                var cacheConfig = new GenericCacheConfig { Limit = 40 };
-                return new GenericCache<K,V>(cacheConfig, provider.GetService<ILogger<GenericCache<K,V>>>());
-            });
-
-
-            //services.AddSingleton(config);
-            //services.AddSingleton<IGenericCache<K,V>, GenericCache<K,V>>();
+            services.AddSingleton(config);
+            services.AddSingleton<IGenericCache<K, V>, GenericCache<K, V>>();
             return services;
-        }
-
-        
+        } 
     }
 }
